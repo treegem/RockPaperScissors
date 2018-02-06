7eb8aec01d0c63678e752b89043e6e5e2a56e1b2
h4ckamuenster <h4ckamuenster@web.de>
2018-02-06 23:55:45 +0100

    validate_choices working, make_random_choice working

--- src/rockpaperscissors.py
+++ src/rockpaperscissors.py
@@ -1,9 +1,13 @@
+from random import randint
+
+
 class RockPaperScissors:
     def __init__(self):
-        self.human_choice = None
-        self.computer_choice = self.make_computer_choice()
-        self.rules = self.make_rules()
         self.valid_choices = ['Scissors', 'Paper', 'Rock', 'Lizard', 'Spock']
+        self.rules = self.make_rules()
+        self.human_choice = None
+        self.computer_choice = None
+        self.make_computer_choice()
 
     def compare(self):
         self.validate_choices()
@@ -18,9 +22,16 @@ class RockPaperScissors:
                             'Human choice: {}, Computer choice: {}'.format(self.human_choice, self.computer_choice))
 
     def make_computer_choice(self):
-        pass
+        choice = self.make_random_choice()
+        self.computer_choice = choice
 
-    def make_rules(self):
+    def make_random_choice(self):
+        choice_index = randint(0, len(self.valid_choices) - 1)
+        choice = self.valid_choices[choice_index]
+        return choice
+
+    @staticmethod
+    def make_rules():
         return ['Scissors cuts Paper', 'Paper covers Rock', 'Rock crushes Lizard', 'Lizard poisons Spock',
                 'Spock smashes Scissors', 'Scissors decapitates Lizard', 'Lizard eats Paper', 'Paper disproves Spock',
                 'Spock vaporizes Rock', 'Rock crushes Scissors']
--- test/test_rockpaperscissors.py
+++ test/test_rockpaperscissors.py
@@ -11,3 +11,30 @@ def test_validate_choice():
         with pytest.raises(NameError) as e:
             game.validate_choice(choice + 's')
         assert 'Corrupt choices' in str(e.value)
+
+
+def test_validate_choices():
+    for human_choice in game.valid_choices:
+        for computer_choice in game.valid_choices:
+            game.human_choice = human_choice
+            game.computer_choice = computer_choice
+            game.validate_choices()
+    with pytest.raises(NameError) as e:
+        game.human_choice = 'InvalidChoicerito'
+        game.validate_choices()
+    assert 'Corrupt choices' in str(e.value)
+
+
+def test_make_random_choice():
+    computed_choices = []
+    i = 0
+    safety_break = 1e4
+    while not set(computed_choices) == set(game.valid_choices):
+        random_choice = game.make_random_choice()
+        if random_choice not in computed_choices:
+            assert random_choice in game.valid_choices
+            computed_choices.append(random_choice)
+        i += 1
+        if i == safety_break:
+            break
+    assert set(computed_choices) == set(game.valid_choices)

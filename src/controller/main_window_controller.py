b1f3450f1f9ed54d1c912d6bde0f738249b9d724
gbrown <georg.braunbeck@googlemail.com>
2018-02-07 17:17:38 +0100

    implemented button clicking

--- src/controller/main_window_controller.py
+++ src/controller/main_window_controller.py
@@ -1,9 +1,23 @@
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QDialog
 
+from src import rockpaperscissors
+
 
 class MainWindow(QDialog):
     def __init__(self):
         super(MainWindow, self).__init__()
 
+        self.game = rockpaperscissors.RockPaperScissors()
+
         self.ui = loadUi('../gui/main_window.ui', self)
+        self.ui.btn_rock.clicked.connect(lambda: self.play_round('Rock'))
+        self.ui.btn_paper.clicked.connect(lambda: self.play_round('Paper'))
+        self.ui.btn_scissors.clicked.connect(lambda: self.play_round('Scissors'))
+        self.ui.btn_lizard.clicked.connect(lambda: self.play_round('Lizard'))
+        self.ui.btn_spock.clicked.connect(lambda: self.play_round('Spock'))
+
+    def play_round(self, human_choice):  # TODO: move to game class
+        self.game.human_choice = human_choice
+        self.game.make_computer_choice()
+        winner = self.game.compare()
